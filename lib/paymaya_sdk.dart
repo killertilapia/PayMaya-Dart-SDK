2855dbf45a15d03173bb546829671a47f4ea9a5f
Jay Ginete <jginete@innovuze.com>
2019-12-19 14:14:18 +0800

    add pubspec.yaml

---
+++ lib/paymaya_sdk.dart
@@ -0,0 +1,1 @@
+library paymaya_sdk;
---
+++ lib/src/constants.dart
@@ -0,0 +1,24 @@
+const SDK_NAME = "Paymaya-Dart-SDK";
+const SDK_VERSION = "0.0.1";
+const SDK_SIGNITURE = "PayMaya-Dart-SDK 0.0.1";
+
+// Environments
+const PRODUCTION = "PRODUCTION";
+const SANDBOX = "SANDBOX";
+
+// Payments Endpoints
+const PAYMENTS_SANDBOX_URL = "https://pg-sandbox.paymaya.com/payments";
+const PAYMENTS_PRODUCTION_URL = "https://pg.paymaya.com/payments";
+const PAYMENTS_TOKEN_URL = "/v1/payment-tokens";
+const PAYMENTS_URL = "/v1/payments";
+
+// Payment Vault
+const PAYMENT_VAULT_CUSTOMERS = "/v1/customers";
+
+// Checkout Endpoints
+const CHECKOUT_SANDBOX_URL = "https://pg-sandbox.paymaya.com/checkout";
+const CHECKOUT_PRODUCTION_URL = "https://pg.paymaya.com/checkout";
+const CHECKOUTS_URL = "/v1/checkouts";
+
+// Redirect URLS
+const REDIRECT_URLS = {"success": "", "failure": "", "cancel": ""};
